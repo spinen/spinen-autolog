a27fd977564cd8f384e373ca12fab7a4d029a435
Luke Reimer <luke.reimer@spinen.com>
2015-04-03 13:46:09 -0400

    added the nolostkill line as an attribute

---
+++ attributes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: @cookbook_name
+# Atributes:: default
+#
+# Copyright (C) 2015 SPINEN
+#
+# Licensed under the MIT license.
+# A copy of this license is provided at the root of this cookbook.
+#
+default['autolog']['nolostkill'] = true
