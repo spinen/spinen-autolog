c2854375ab9ed9e13c8e914abb3faf0405df3be7
Luke Reimer <luke.reimer@spinen.com>
2015-04-08 13:56:41 -0400

    bumped version no.

--- metadata.rb
+++ metadata.rb
@@ -3,9 +3,10 @@ maintainer 'Luke Reimer'
 maintainer_email 'luke.reimer@spinen.com'
 license 'MIT'
 description 'Installs/Configures autolog'
-version '0.1.0'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+
+version IO.read(File.join(File.dirname(__FILE__), 'VERSION'))
 recipe 'default', 'Installs autolog'
 recipe 'vagrant', 'includes the default but also excludes the vagrant user from being logged out'
 
 supports 'ubuntu'
-
